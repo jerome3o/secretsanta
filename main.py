c8d56b077dab08a478cda4d9d7f511ca9e46a5d8
Jerome Swannack <jeromeswannack@gmail.com>
2023-12-14 16:35:21 +1300

    Updated list, no niamhy :'(

--- main.py
+++ main.py
@@ -2,37 +2,41 @@ from pathlib import Path
 from random import shuffle
 
 _people = [
-    "jane",
-    "cole",
-    "lindsay",
-    "niamh",
-    "jerome",
-    "liv",
-    "harriet",
-    "rowan",
-    "kayla",
-    "shane",
-    "audrey",
+    "Audrey",
+    "Shane",
+    "Kayla",
+    "Jane",
+    "Lindsay",
+    "Cole",
+    "Harriet",
+    "Rowan",
+    "Jerome",
+    "Olivia",
 ]
 _output_dir = Path("outputs")
 _output_dir.mkdir(exist_ok=True, parents=True)
+
 
 def main():
     shuffle(_people)
     pairs = [
         (santa, recipient)
-        for santa, recipient
-        in zip(
+        for santa, recipient in zip(
             [_people[-1]] + _people[:-1],
             _people,
         )
     ]
     with open("template.txt", "r") as f:
         template = f.read()
-    
+
     for santa, recipient in pairs:
         with open(_output_dir / f"{santa}.txt", "w") as f:
-            f.write(template.format(santa=santa.capitalize(), recipient=recipient.capitalize()))
+            f.write(
+                template.format(
+                    santa=santa.capitalize(), recipient=recipient.capitalize()
+                )
+            )
+
 
 if __name__ == "__main__":
     import logging
